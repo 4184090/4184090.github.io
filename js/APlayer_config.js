8e2e0a0f45f398f80de715617a704e9e7da01b8e
4184090 <4184090@qq.com>
2020-04-30 18:00:23 +0800

    Site updated: 2020-04-30 18:00:20

--- js/APlayer_config.js
+++ js/APlayer_config.js
@@ -2,7 +2,7 @@ const ap = new APlayer({
     container document.getElementById('aplayer'),
     fixed true,
     autoplay false,
-    theme '#b7daff',
+    theme '#FADFA3',
     loop 'all',
     order 'list',
     preload 'auto',
@@ -10,13 +10,14 @@ const ap = new APlayer({
     lrcType 3,
     mutex true,
     listFolded false,
-
+	listMaxHeight: 90,
     audio [{
             name '椿',
             artist '沈以诚',
             lrc 'hppt://it-max.cn/music/椿-沈以诚.lrc',
             cover 'hppt://it-max.cn/music/椿-沈以诚.jpg',
             url 'hppt://it-max.cn/music/椿-沈以诚.mp3'
-                  }
+			theme: '#ebd0c2'
+            }
         ]
 });
